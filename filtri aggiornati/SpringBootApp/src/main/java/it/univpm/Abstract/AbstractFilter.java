ed746cce2790f61bf15f5531ce2a14b118e9bccf
giadarem <giadaremedia@gmail.com>
2021-12-31 16:45:02 +0100

    commit filtri aggiornati

---
+++ filtri aggiornati/SpringBootApp/src/main/java/it/univpm/Abstract/AbstractFilter.java
@@ -0,0 +1,25 @@
+package it.univpm.Abstract;
+
+import it.univpm.ArrayLists.ArrayListTweetPost;
+import org.json.*;
+import java.util.ArrayList;
+import it.univpm.Model.TweetPost;
+
+public abstract class AbstractFilter {
+    protected String search_fields;
+
+    public AbstractFilter(String search_fields) {
+        setFields(search_fields);
+    }
+
+    public String getFields() {
+        return search_fields;
+    }
+    public void setFields(String search_fields) {
+        this.search_fields = search_fields;
+    }
+
+    public abstract boolean searchElement(TweetPost elem);
+    public abstract ArrayListTweetPost getFound(ArrayListTweetPost list);
+
+}
